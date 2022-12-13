25e001f6b87b7b0b2917ccffd46b040034a7a5c3
Petr Prokhorenkov <prokhorenkov@gmail.com>
2022-12-14 00:26:48 +0100

    An obfuscated python solution for day 11

---
+++ petrprokhorenkov/11.py
@@ -0,0 +1,81 @@
+(lambda _,i,l,I,o,_i,ii,li,Ii,oi,_l,il,ll: (lambda II,oI,_o,io:(lambda Io,oo:(lambda __i,i_i:(lambda
+l_i:(lambda I_i:(lambda _ii: (lambda oii: (lambda _li,ili,lli,Ili, oli,_Ii,iIi,lIi,IIi,oIi, _oi,ioi,
+loi,Ioi,ooi,__l,i_l,l_l,I_l,o_l,_il, iil: (lambda lil,Iil, oil, _ll,ill,lll,Ill,oll,_Il,iIl,lIl,IIl:
+(lambda oIl, _ol: (lambda Iol,ool: (lambda __I: (lambda i_I: (lambda l_I,I_I: (lambda o_I, _iI, iiI,
+liI, IiI, oiI: (lambda _lI, ilI: (lambda olI, _II, iII,lII: (lambda III: (Ii(lambda lii: lambda oII:
+lambda _oI: _lI(IiI(l_i, len(_oI)), lambda:( lambda ioI: (oII.append(l( ( ii(int, io( io(_oI).split(
+oIl(_il))).split(oIl(iIi) )), _lI(liI(_o(ioI), oIl(oli)), lambda: _lI(liI(io(ioI),oIl(_ll)), lambda:
+(oI,II,II), lambda: (II, int(io(ioI)), II)), lambda: (II,oI, int(io(ioI)))), _II( i_I(_oI, __i)),_II
+(i_I(_oI,i_i)), _II(i_I(_oI, l_i)),l()))), lii(lii)(oII)(I_I( _oI))))(l_I(io(i_I(_oI, oo).split(oIl(
+iil)) ).split())), lambda: ()))(III)(l(ool(lll ).stdin)), ii(lambda loI: (lambda III, IoI: ii(lambda
+ooI: ((lambda next:  ii(lambda __o : _o(i_I(III, i_I(ooI, o_I(i_i, io(__o))))).append(_o(__o)), zip(
+next, (lambda lI, i_o: ii (lambda l_o: liI(oiI(_o(l_o), io(l_o)), II), zip(lI, i_o)))(next, iII(i_I(
+ooI, oo), next )))))(ii(_o(olI), (lambda IoI, lI, i_o: ii(lambda I_o: oiI(sum(map(lambda o_o:_iI(_o(
+o_o),io(o_o)), zip(I_o, i_o))), IoI), lI))(IoI, zip(map(o(ll( _iI))(I),_o(ooI)),_o(ooI),iII( oI, _o(
+ooI))),io(ooI)))), i_I(ooI, l_i).append(len(_o(ooI))),_o(ooI).clear()),III))(III,lII( ii(lambda _io:
+i_I(_io,oo),III))), range(io(olI))), print((lambda iio: _iI(_o(iio),io(iio )))(Ii(lambda lii: lambda
+Iii:lambda lI:_lI(liI(len(lI),II),lambda: Iii,lambda:(lambda lio: _lI( IiI(_o(lI), _o(Iii)), lambda:
+lio(Iii),lambda:_lI(IiI( _o(lI), io(Iii)), lambda: lio(( _o(lI), io(Iii))),lambda: lio((io(Iii), _o(
+lI))))))(lambda Iio:lii(lii)(Iio)(l_I(lI)))))((II,II))(ii(lambda oio:sum(i_I(oio, l_i)), III))))))(l
+()))(_lI(liI(io(Iol(ool(lll),_Il)),str(oI)), li((il(iiI, __i), ooi)), li((I, oii(oli, i_i)))),lambda
+Il: int(i_I(Il.split(), Io)),lambda lol, lI: _iI((lol, ),len(lI)),Ii(lambda lii: lambda lI:_lI(bool(
+lI), lambda: ilI(_o(lI), lii(lii)(l_I(lI))) , lambda: oI))))((lambda I, llI, IlI: i_I((IlI, llI), I)
+()),ool(oll).lcm))(Iol(__I,oil),Iol(__I, ill),Iol(__I, IIl),Iol(__I, lil),Iol(__I, Iil),Iol(__I, Ill
+)))(il(i_I, slice(oI, Iil)),il(i_I, slice(ili, Iil))))(Iol(__I, iIl)))(ool(lIl)))(lambda Il, iI: oi(
+_ol(Il), oIl)(iI),oi(_, oIl)))((lambda Il:i.join((lambda iol: (lambda lo: iol)(Ii(lambda lii: lambda
+iol,lol:I_i(lol, lambda: (lambda Il: (iol.append(chr(_l(io(Il), oii(oo, l_i)))), lii(lii)(iol, _o(Il
+))))(divmod(lol,_ii(Ili, oli))),))(iol, Il)))(l()))),ll(getattr)))(sum((oii(l_i, l_i), oii(l_i, l_i)
+, oii(l_i, i_i),oii(l_i, __i), oii(l_i, __i), oii(l_i, __i), oii(l_i, oo), oii(l_i, oo), oii(l_i, oo
+),oii(l_i, oo), l_i, __i, oI)),sum((oii(l_i, l_i), oii(l_i, l_i), oii(l_i, i_i), oii(l_i, i_i), oii(
+l_i,__i),l_i,l_i, oI)),sum((oii(l_i, lli), oii(l_i, ili), oii(l_i, _li), oii(l_i, _li), oii(l_i, _li
+), oii(l_i, _li),oii(l_i, l_i), oii(l_i, l_i), oii(l_i, l_i), oii(l_i, i_i), oii(l_i, i_i), oii(l_i,
+__i),oii(l_i,__i), oii(l_i, __i), oii(l_i, oo), oii(l_i, oo), oii(l_i, oo), l_i, __i)),sum((oii(l_i,
+lli), oii(l_i, ili), oii(l_i, ili), oii(l_i, l_i), oii(l_i, l_i), oii(l_i, l_i), oii(l_i, i_i), oii(
+l_i,i_i), oii(l_i, __i), oii(l_i, oo), oii(l_i, oo), oii(l_i, oo), l_i, l_i, l_i, __i, oI)),sum((oii
+(l_i, lli), oii(l_i, ili),oii(l_i, ili), oii(l_i, _li), oii(l_i, _li), oii(l_i, _li), oii(l_i, _li),
+oii(l_i,l_i),oii(l_i,l_i),oii(l_i, l_i), oii(l_i, l_i), oii(l_i, i_i), oii(l_i, i_i), oii(l_i, __i),
+oii(l_i,oo),oii(l_i, oo),oii(l_i,oo), oo)),sum((oii(l_i, lli), oii(l_i,ili), oii(l_i, ili), oii(l_i,
+ili), oii(l_i, _li), oii(l_i, _li), oii(l_i, _li), oii(l_i, l_i), oii(l_i, l_i), oii(l_i, i_i), oii(
+l_i, i_i), oii(l_i, i_i), oii(l_i, __i),oii(l_i, __i), oii(l_i, __i), l_i, l_i, l_i, __i)),sum((oii(
+l_i, _Ii), oii(l_i, lli), oii(l_i, lli), oii(l_i, lli), oii(l_i, ili), oii(l_i, ili), oii(l_i, _li),
+oii(l_i, _li), oii(l_i, _li), oii(l_i,_li),oii(l_i,__i), oii(l_i, oo), __i)),sum((oii(l_i, _Ii), oii
+(l_i, Ili), oii(l_i, Ili), oii(l_i, lli), oii(l_i, _li), oii(l_i, _li),oii(l_i, _li), oii(l_i, i_i),
+oii(l_i, i_i), oii(l_i, i_i), oii(l_i, __i), oii(l_i, __i),oii(l_i,__i),oii(l_i, __i), oii(l_i, oo),
+oii(l_i, oo), oii(l_i, oo), oo)),sum((oii(l_i, _Ii), oii(l_i, oli),oii(l_i,Ili), oii(l_i, Ili), oii(
+l_i, lli), oii(l_i, ili), oii(l_i, ili), oii(l_i, ili), oii(l_i, ili), oii(l_i, _li), oii(l_i, _li),
+oii(l_i, _li), oii(l_i, _li), oii(l_i, l_i), oii(l_i, l_i), oii(l_i,l_i),oii(l_i, i_i), oii(l_i, i_i
+), oii(l_i, __i), oii(l_i, __i), oii(l_i, oo), l_i, l_i, l_i,l_i)),sum((oii(l_i, Ioi), oii(l_i, Ioi)
+, oii(l_i, ioi), oii(l_i, ioi), oii(l_i, ioi), oii(l_i, ioi), oii(l_i, oIi), oii(l_i, oIi), oii(l_i,
+oIi ), oii(l_i, oIi), oii(l_i, IIi), oii(l_i, IIi), oii(l_i, iIi), oii(l_i, iIi), oii(l_i, iIi), oii
+(l_i, _Ii), oii(l_i, _Ii), oii(l_i, _Ii), oii(l_i, _Ii), oii(l_i, oli), oii(l_i, oli), oii(l_i, Ili)
+, oii(l_i, Ili), oii(l_i, lli), oii(l_i, lli), oii(l_i, lli), oii(l_i, ili), oii(l_i, l_i), oii(l_i,
+l_i), oii(l_i, i_i), oii(l_i, i_i), oii(l_i, i_i), oii(l_i, i_i), oii(l_i, __i), oii(l_i, __i), oii(
+l_i,__i),oii(l_i,__i), oii(l_i, oo), oii(l_i, oo), l_i, oI)),sum((oii(l_i, i_l), oii(l_i, __l), oii
+(l_i, __l), oii(l_i,__l), oii(l_i, ooi), oii(l_i, Ioi), oii(l_i, Ioi), oii(l_i, loi), oii(l_i, loi),
+oii(l_i,loi),oii(l_i, loi), oii(l_i, ioi), oii(l_i, _oi), oii(l_i, oIi), oii(l_i, oIi), oii(l_i, oIi
+), oii(l_i, oIi), oii(l_i,IIi), oii(l_i, lIi), oii(l_i, lIi), oii(l_i, lIi), oii(l_i, iIi), oii(l_i,
+iIi), oii(l_i, iIi), oii(l_i, _Ii), oii(l_i, _Ii), oii(l_i, _Ii), oii(l_i, _Ii), oii(l_i, Ili), oii(
+l_i,Ili),oii(l_i,lli),oii(l_i, lli), oii(l_i, lli), oii(l_i, lli), oii(l_i, ili), oii(l_i, _li), oii
+(l_i,_li),oii(l_i,_li), oii(l_i, __i), oii(l_i, __i), oii(l_i, __i), oii(l_i, oo), oii(l_i, oo), __i
+, oI)),sum((oii(l_i,o_l), oii(l_i, I_l), oii(l_i, l_l), oii(l_i, i_l), oii(l_i, __l), oii(l_i, __l),
+oii(l_i, __l),oii(l_i,ooi), oii(l_i, ooi), oii(l_i, ooi), oii(l_i, Ioi), oii(l_i, Ioi), oii(l_i, Ioi
+), oii(l_i, Ioi), oii(l_i,loi), oii(l_i, _oi), oii(l_i, oIi), oii(l_i, oIi), oii(l_i, oIi), oii(l_i,
+oIi), oii(l_i, IIi), oii(l_i, IIi), oii(l_i, lIi), oii(l_i, lIi), oii(l_i, iIi), oii(l_i, oli), oii(
+l_i, oli), oii(l_i, oli), oii(l_i, oli), oii(l_i, lli), oii(l_i, lli), oii(l_i, _li), oii(l_i, l_i),
+oii(l_i, l_i), oii(l_i, l_i),oii(l_i,l_i), oii(l_i, i_i), oii(l_i, i_i), oii(l_i, i_i), oii(l_i, i_i
+),oii(l_i,__i), oii(l_i, __i), oii(l_i,__i),oii(l_i, oo), oii(l_i, oo), oii(l_i, oo), l_i, l_i, l_i,
+l_i, __i))))(sum((l_i, oI)),sum((l_i, oo)),sum((l_i, __i)),sum((l_i, __i, oI)),sum((l_i, l_i)),sum((
+l_i, l_i, oI)),sum((l_i,l_i, oo)),sum((l_i, l_i, __i)),sum((l_i, l_i, __i, oI)),sum((l_i, l_i, l_i))
+,sum((l_i,l_i, l_i, oI)),sum((l_i, l_i, l_i, oo)),sum((l_i, l_i, l_i, __i)),sum((l_i, l_i, l_i, __i,
+oI)),sum((l_i, l_i, l_i, l_i)),sum((l_i, l_i, l_i, l_i, oI)),sum((l_i, l_i, l_i, l_i, oo)),sum((l_i,
+l_i, l_i, l_i, __i)),sum((l_i,l_i, l_i, l_i, __i, oI)),oii(l_i, oo),sum((oii(l_i, oo), oI)),sum((oii
+(l_i,oo),__i, oI))))(lambda Il,iii: (lambda l: _i( Ii(lambda lii: lambda Iii, iii: I_i( iii, lambda:
+_i(l.insert(II,Iii),lii(lii)(_ii(Iii, Il), _l(iii, Io)))))(Il, iii), _o(l)))(l((oI,)))))((lambda Il,
+iii:(lambda l: _i( Ii(lambda lii: lambda Iii, iii: I_i( iii, lambda: _i( l.insert(II, Iii), lii(lii)
+(_l(Iii,Il), _l(iii, Io)))))(Il, iii), _o(l)))(l((II,))))))(lambda o_i, ol: any(map(lambda ol: ol(),
+(lambda:(lambda Il: abs(_l(Io, bool(Il))))(o_i), lambda: _i(ol(), oI))))))(_l(__i, oo)))(_l(oo, oI),
+_l(oo, oo)))(i.find( chr(oI)),_l(oI, oI)))(len(()),len(((),)),oi(next, iter),lambda lI: (lambda I: (
+lambda lo:next(I))(next(I)))(iter(lI))))(__import__,str(),list,(lambda Il: Il),(lambda ol: lambda _I
+:lambda Il:ol(Il)(_I(Il))),(lambda Il,iI: iI),(lambda ol, lI: list(map(ol, lI))), (lambda Il:lambda:
+Il),(lambda ol:ol(ol)),(lambda ol, _I: lambda Il: ol(_I(Il))),(lambda Il, iI: sum((Il, iI))),(lambda
+    ol,iI: lambda Il: ol(Il, iI)),(lambda ol: lambda Il: lambda iI: ol(Il, iI)))
